c419f1a348ced05ba4a93d0bd443c66275624b75
moe001 <i@001.moe>
2018-06-27 06:10:26 +0800

    初始版本完成

---
+++ moelog_main/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(name='moelog',
+      version='0.0.0.1',
+      description='A easy to use Logger',
+      author='moe001',
+      author_email='i@001.moe',
+      url='https://github.com/moe001/moelog',
+      packages=find_packages(),
+      license='MIT',
+      long_description=open('README.rst').read(),
+     )
